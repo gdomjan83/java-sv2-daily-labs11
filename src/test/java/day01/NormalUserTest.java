3ff97ec19bbb73d5e725456baf73b02bd77401d6
J_C <gomjan83@gmail.com>
2022-01-03 20:20:21 +0100

    Day01 excercise finished.

---
+++ src/test/java/day01/NormalUserTest.java
@@ -0,0 +1,21 @@
+package day01;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class NormalUserTest {
+    @Test
+    void testNormalUserCreate() {
+        NormalUser normalUser = new NormalUser("gdomj@hotmail.com", "goodpassword");
+        assertEquals("gdomj@hotmail.com", normalUser.getUserEmail());
+        assertEquals("goodpassword", normalUser.getPassword());
+    }
+
+    @Test
+    void testAdminUserCreate() {
+        AdminUser adminUser = new AdminUser("admin_main@hotmail.com", "adminpass");
+        assertEquals("admin_main@hotmail.com", adminUser.getUserEmail());
+        assertEquals("*********", adminUser.getPassword());
+    }
+}
