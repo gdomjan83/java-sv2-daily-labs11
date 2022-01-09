312d0c0cd8ccf5f03743fd9445670d6265a1ed0d
J_C <gomjan83@gmail.com>
2022-01-09 11:23:41 +0100

    Day05 excercise updated.

--- src/test/java/day05/LotteryTest.java
+++ src/test/java/day05/LotteryTest.java
@@ -8,11 +8,15 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class LotteryTest {
 
-    Lottery lottery = new Lottery(90, 5, new Random(666));
-
     @Test
     void testStartLottery() {
+        Lottery lottery = new Lottery(90, 5, new Random(666));
         assertEquals(5, lottery.startLottery().size());
     }
 
+    @Test
+    void testStartLottery2() {
+        Lottery lottery = new Lottery(5, 5, new Random(666));
+        assertEquals(5, lottery.startLottery().size());
+    }
 }
